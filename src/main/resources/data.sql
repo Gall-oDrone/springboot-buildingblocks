f57a134b15529bb68d61a1825ea02fc5f2201a1e
dgallovalenzuela <dgallovalenzuela@host.docker.internal>
2022-12-05 19:44:41 -0600

    JPA Base RestServices Development Completed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(101, 'kreddy1@gmail.com', 'Kay', 'Reddy','admin', 'ssn101', 'kreddy');
+insert into user values(102, 'gwiser@gmail.com', 'Gre', 'Wiser','admin', 'ssn102', 'gwiser');
+insert into user values(103, 'dmark@gmail.com', 'Dav', 'Mark','admin', 'ssn103', 'dmark');
